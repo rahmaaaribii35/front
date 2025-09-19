9a5f8e480756f0eddec9810f060435976624690c
Rahma Aribi <rahmaaribi35@gmail.com>
2025-09-19 21:09:16 +0100

    add some product

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -46,6 +46,82 @@ import p102 from './p10-2.jpg'
 import p103 from './p10-3.jpg'
 import p104 from './p10-4.mp4'
 import p105 from './p10-5.jpg'
+import p111 from './p11.1.jpeg'
+import p112 from './p11.2.jpeg'
+import p113 from './p11.3.jpeg'
+import p114 from './p11.4.jpeg'
+import p115 from './p11.5.jpeg'
+import p121 from './p12-1.jpeg'
+import p122 from './p12-2.jpeg'
+import p123 from './p12-3.jpeg'
+import p124 from './p12-4.jpeg'
+import p125 from './p12-5.jpeg'
+import p126 from './p12-6.jpeg'
+import p131 from './p13-1.webp'
+import p132 from './p13-2.webp'
+import p133 from './p13-3.webp'
+import p134 from './p13-4.webp'
+import p141 from './p14-1.jpg'
+import p142 from './p14-2.jpg'
+import p151 from './p15-1.jpg'
+import p152 from './p15-2.jpg'
+import p161 from './p16-1.jpg'
+import p162 from './p16-2.jpg'
+import p163 from './p16-3.mp4'
+import p171 from './p17-1.jpg'
+import p172 from './p17-2.jpg'
+import p173 from './p17-3.mp4'
+import p181 from './p18-1.jpg'
+import p182 from './p18-2.mp4'
+import p191 from './p19-1.jpg'
+import p192 from './p19-2.jpg'
+import p193 from './p19-3.jpg'
+import p194 from './p19-4.jpg'
+import p195 from './p19-5.jpg'
+import p201 from './p20-1.jpg'
+import p202 from './p20-2.jpg'
+import p203 from './p20-3.jpg'
+import p204 from './p20-4.jpg'
+import p205 from './p20-5.jpg'
+import p211 from './p21-1.jpg'
+import p212 from './p21-2.jpg'
+import p213 from './p21-3.mp4'
+import p221 from './p22-1.jpg'
+import p222 from './p22-3.jpg'
+import p231 from './p23-1.jpg'
+import p232 from './p23-2.jpg'
+import p233 from './p23-3.jpg'
+import p234 from './p23-4.jpg'
+import p241 from './p24-1.jpg'
+import p242 from './p24-2.jpg'
+import p243 from './p24-3.jpg'
+import p244 from './p24-4.jpg'
+import p251 from './p25-1.jpg'
+import p252 from './p25-2.jpg'
+import p253 from './p25-3.mp4'
+import p261 from './p26-1.jpeg'
+import p262 from './p26-2.jpeg'
+import p263 from './p26-3.jpeg'
+import p264 from './p26-4.jpeg'
+import p265 from './p26-5.mp4'
+import p271 from './p27-1.jpg'
+import p272 from './p27-2.jpg'
+import p273 from './p27-3.jpg'
+import p274 from './p27-4.jpg'
+import p275 from './p27-5.jpg'
+import p281 from './p28-1.jpg'
+import p282 from './p28-2.jpg'
+import p283 from './p28-3.jpg'
+import p291 from './p29-1.jpg'
+import p292 from './p29-2.jpg'
+import p293 from './p29-3.jpg'
+import p294 from './p29-4.jpg'
+import p295 from './p29-5.mp4'
+import p301 from './p30-1.jpeg'
+import p302 from './p30-2.jpeg'
+import p303 from './p30-3.jpeg'
+import p304 from './p30-4.jpeg'
+
 
 import exchangeIcon from './exchange-icon.jpeg'
 import qualityIcon from './quality-icon.jpeg'
@@ -58,7 +134,7 @@ import crossIcon from './cross-icon.jpeg'
 
 
 export const assets={
-    logo , searchIcon , profileIcon , cartIcon , menuIcon , dropdownIcon , Carousel1 , Carousel2 , heroVideo, p11 , p12 , p13 , p14 , p15 , p16 , p21 , p22 , p23 , p24 , p25 , p31 , p32 , p33 , p34  , p41 , p42 , p43 , p51 , p52 , p53 , p61 , p62 , p71 , p72 , p73 , p81 , p82 , p83 , p91 , p92 , p93 , p94 , p101 , p102 , p103 , p104 , p105 , exchangeIcon , qualityIcon , supportIcon , crossIcon
+    logo , searchIcon , profileIcon , cartIcon , menuIcon , dropdownIcon , Carousel1 , Carousel2 , heroVideo, p11 , p12 , p13 , p14 , p15 , p16 , p21 , p22 , p23 , p24 , p25 , p31 , p32 , p33 , p34  , p41 , p42 , p43 , p51 , p52 , p53 , p61 , p62 , p71 , p72 , p73 , p81 , p82 , p83 , p91 , p92 , p93 , p94 , p101 , p102 , p103 , p104 , p105 ,p111 ,p112 ,p113 , p114,p115 ,p121 ,p122,p123,p124,p125,p126 , p131,p132,p133,p134 ,p141,p142,p151,p152,p161,p162,p163,p171,p172,p173,p181,p182,p191,p192,p193,p194,p195,p201,p202,p203,p204,p205,p211,p212,p213, p221,p222,p231,p232,p233,p234,p241,p242,p243,p244,p251,p252,p253,p261,p262,p263,p271,p272,p273,p274,p275,p264,p265,p271,p272,p273,p274,p275,p281,p282,p283,p291,p292,p293,p294,p301,p302,p303,p304, exchangeIcon , qualityIcon , supportIcon , crossIcon
 }
 
 export const products=[
@@ -163,7 +239,207 @@ export const products=[
         images: [p101 , p102 , p103 , p104 , p105],
         bestseller:true
     },
-
+    {
+        _id: 11,
+        name: " Lashlighter Up & Out Mascara",
+        description: "Your makeup routine just got brighter! Our Lashlighter Up & Out Mascara is here to lift, lengthen, and light up your look in one smooth swipe. Its feather-comb brush is expertly designed to hug your eye shape, while three rows of micro-tooth combs catch and coat every lash from root to tip. The result? A fanned-out, fluttery finish with zero clumps. Infused with peptides and jojoba oil, this lash-loving formula nourishes and protects while you wear it, so your lashes look better now and later. Plus, it's waterproof, so your look stays bright-eyed throughout the day. Long, lifted lashes with zero effort? That's what we call a highlight.",
+        price: 11.5,
+        quantity: 19,
+        category: "eye",
+        images: [p111 , p112 , p113 , p114 , p115],
+        bestseller:false
+    },
+    {
+        _id: 12,
+        name: " Color Bloom Liquid Blush-Lilac Dream",
+        description: "YGet flushed with our Color Bloom Liquid Blush, featuring nourishing ingredients and a long-wearing, lightweight gel-cream formula that loves your skin. A sponge tip applicator applies highly pigmented, matte color like a dream and leaves your cheeks pretty 'n plump!",
+        price: 21.5,
+        quantity: 19,
+        category: "face",
+        images: [p121 , p122 , p123 , p124 , p125 , p126],
+        bestseller:true
+    },
+    {
+        _id: 13,
+        name: "Skin Magnet High Coverage Foundation Stick",
+        description: "If you're looking for a portable and easy-to-use foundation, you've come to the right place. Our SHEGLAM Skin Magnet High Coverage Foundation Stick adheres to your skin with magnet-like strength and locks in your look!Featuring a technology-driven formula, our foundation stick provides a stunning natural skin-like finish that won't cake up. Plus, this unique formula is made up of various oils with branched-chain structures, providing a breathable and light texture that doesn't melt off your skin. And in case you need to get ready on the go, its portable compact twist-to-reveal packaging makes it the ultimate travel makeup companion!",
+        price: 30.99,
+        quantity: 19,
+        category: "face",
+        images: [p131 , p132 , p133 , p134],
+        bestseller:false
+    },
+    {
+        _id: 14,
+        name: "Mello Jello Nourishing Lip Balm-Garnet",
+        description: "Stop neglecting your lips during your skincare routine and add this balm to your rotation! Featuring an ultra-nourishing formula, made with vitamin E, jojoba seed oil, and more, this lip balm has a gorgeous colored crystal look that appears clear on the lips. Bring it everywhere and anywhere to ensure smooth and hydrated lips all day!",
+        price: 10,
+        quantity: 19,
+        category: "lips",
+        images: [p141 , p142 ],
+        bestseller:false
+    },
+    {
+        _id: 15,
+        name: "Hold 'Em Up Tinted Brow Gel",
+        description: "Lock in your brow look all day long with our Hold 'Em Up Tinted Brow Gel! Featuring a slick weightless formula, this brow gel shapes, lifts, and sets your brow hairs in place for a flexible hold that ACTUALLY lasts. It is long-wearing and smooth, with a subtle hint of tint for the ultimate laminated brow look.",
+        price: 19.4,
+        quantity: 19,
+        category: "eye",
+        images: [p151 , p152 ],
+        bestseller:false
+    },
+    {
+        _id: 16,
+        name: "Insta-Ready Face & Under Eye Setting Powder Duo-Taro",
+        description: "You and your glam have got a good thing going. Don't let it fade by adding our 2-in-1 setting powder to your beauty routine. On the top level you'll find a silky moisturizing pressed powder that's uniquely made to lock in your under eye makeup (and fill in any wrinkles),and on the bottom level, a sheer, oil-absorbing loose powder to keep your face fresh and shine-free!",
+        price: 22.9,
+        quantity: 19,
+        category: "face",
+        images: [p161 , p162, p163],
+        bestseller:false
+    },
+    {
+        _id: 17,
+        name: "Jelly-Licious Hydrating Lip & Blush Tint-Howdy",
+        description: " Are you ready for this jelly? Our SHEGLAM Jelly-licious Tinted Blush Stick features a bouncy jelly texture and a long-lasting formula that applies seamlessly for a stunning sheer natural flush you'll love.",
+        price: 12.9,
+        quantity: 19,
+        category: "face",
+        images: [p171 , p172, p173],
+        bestseller:false
+    },
+    {
+        _id: 18,
+        name: "Glacier Glo Smoothing Primer",
+        description: "When your skin needs an extra dose of hydration before your glam, reach for our SHEGLAM Glacier Glo Smoothing Primer! Featuring a smooth water-based texture that's cool to the touch, this primer fills pores and develops a soft focus isolation layer that fights oxidation over time and reduces oiliness during the day.",
+        price: 40.9,
+        quantity: 19,
+        category: "face",
+        images: [p181 , p182,],
+        bestseller:true
+    },
+    {
+        _id: 19,
+        name: "Pout-Perfect Shine Lip Plumper",
+        description: "FINALLY! A gloss with all of the PLUMP and none of the PAIN! This solid lip gloss stick delivers high-shine and amazing color saturation for the juiciest lips ever! Formulated with nourishing coconut oil, our solid lip gloss keeps lip moisturized without the stickiness! Cue all the compliments, because with this gloss, they'll never stop coming!",
+        price: 19,
+        quantity: 19,
+        category: "lips",
+        images: [p191 , p192,p193,p194,p195],
+        bestseller:true
+    },
+    {
+        _id: 20,
+        name: "Glo Hero Bronzing Drops",
+        description: "Suffering from dull skin and lackluster looks? That sounds like a job for Glo Hero! Our SHEGLAM Glo Hero Bronzing Drops feature an antioxidant-rich formula including cocoa extract, Vitamin E derivative, and more, to hydrate and support your skin while providing a stunning bronze sun-kissed glow.",
+        price: 19,
+        quantity: 19,
+        category: "face",
+        images: [p201 , p202,p203,p204,p205],
+        bestseller:false
+    },
+    {
+        _id: 21,
+        name: "Good Grip Hydrating Primer-Blueberry",
+        description: "Buckle in base makeup, because you're not going anywhere! Our silicone-free primer features a refreshing gel texture that makes your skin soft, smooth, and moist. Formulated with fruit extract and AHA, this primer keeps your makeup in place for hours on end and is suitable for your skin.",
+        price: 26.5,
+        quantity: 19,
+        category: "face",
+        images: [p211, p212,p213],
+        bestseller:true
+    },
+    {
+        _id: 22,
+        name: "Hot Streak Thermal Defense Glow Oil Elixir",
+        description: "Recommended Amount: Using 1 pump for short hair and about 2 pumps for medium to long hair is recommended.(The amount can be adjusted based on hair length and volume.)",
+        price: 32.2,
+        quantity: 19,
+        category: "hair",
+        images: [p221, p222],
+        bestseller:false
+    },
+    {
+        _id: 23,
+        name: "Brow-Fection Angled Brush & Dip-Taupe",
+        description: "Fill in sparse brows or create a completely custom brow look with our SHEGLAM Brow-fection Angled Brush & Dip! Featuring an angled square brush and a matte liquid solution, this innovative combo helps you achieve a stunning microblade effect with realistic hair-like strokes.",
+        price: 15.5,
+        quantity: 19,
+        category: "eye",
+        images: [p231, p232, p233, p234],
+        bestseller:false
+    },
+    {
+        _id: 24,
+        name: "Complexion Pro Long Lasting Breathable Matte Foundation Sample-Shell",
+        description: "Meet the SHEGLAM COMPLEXION PRO Long-Lasting Breathable Matte Foundation! This next-level sweat- and waterproof formula is free of fragrances and packed with non pore-clogging ingredients, while also remaining breathable and lightweight on the skin. Each face-loving shade delivers a semi-matte, filter-like finish for a look that's flawless yet natural every time. Wear this long-wearing, buildable foundation to the gym or for a night out with the girls—it's a complexion-perfecting pro no matter where you go!",
+        price: 15.5,
+        quantity: 12,
+        category: "face",
+        images: [p241, p242, p243, p244],
+        bestseller:false
+    },
+    {
+        _id: 25,
+        name: "Pout Pillow Cushion Matte Liquid Lipstick",
+        description: "You loved our original Pout-Pillow Lip Gloss, so you're sure to adore our new lipstick version! This matte liquid lipstick lays on a velvet matte finish with a unique sponge tip applicator. Swipe on a stunning long-wearing lip color that'll stay on throughout the day.",
+        price: 25.9,
+        quantity: 12,
+        category: "lips",
+        images: [p251, p252, p253],
+        bestseller:false
+    },
+    {
+        _id: 26,
+        name: "Rapid Retouch Cream Concealer",
+        description: "Forget the photo editing software and opt for this IRL quick solution instead! Featuring a high coverage, buttery texture, and velvet matte finish, our SHEGLAM Rapid Retouch Cream Concealer melts on your skin, giving it a silky and luxurious skin-like feel! Its hydrophobic and lipophilic properties make it the perfect water-resistant, non-cakey, and transfer-proof full-coverage option for all-day wear.",
+        price: 29,
+        quantity: 12,
+        category: "lips",
+        images: [p261, p262, p263,p264],
+        bestseller:false
+    },
+    {
+        _id: 27,
+        name: "Jelly Wow Tinted Lip Oil",
+        description: "Our new Jelly Wow Tinted Lip Oils feature the same nourishing formula you know and love to deliver a healthy, moisturized shine with a soft hint of tint bringing your pout to life. With a large doe foot applicator, our lip oil's non-greasy texture adds a stunning reflective shine you'll love!",
+        price: 22,
+        quantity: 12,
+        category: "lips",
+        images: [p271, p272, p243,p274,p275],
+        bestseller:false
+    },
+    {
+        _id: 28,
+        name: "Lunar Orbit Blush Ball",
+        description: "Thought you found your favorite way to apply blush? Meet our new SHEGLAM Lunar Orbit Blush Ball! Featuring an innovative hemispherical shape and a smooth silky texture, this blush allows you to achieve a stunning flush in just one swipe! ",
+        price: 11.5,
+        quantity: 12,
+        category: "face",
+        images: [p281, p282, p283],
+        bestseller:false
+    },
+    {
+        _id: 29,
+        name: "All-In-One 24k Multi-Effect Mascara",
+        description: "Unleash the gold standard in lash luxury with SHEGLAM's All-In-One 24K Multi-Effect Mascara. Infused with real 24K gold and a powerful blend of nourishing ingredients-polypeptide, olive oil and orbignya oleifera seed oil-this mascara is crafted to give you the ultimate lash experience. This 2-in-1 formula defines and volumizes lashes while delivering multi-dimensional effects for full-bodied, sky-high lashes that last!",
+        price: 18.5,
+        quantity: 12,
+        category: "eye",
+        images: [p291, p292, p293, p294, p295],
+        bestseller:false
+    },
+    {
+        _id: 30,
+        name: "Crystal Jelly Glaze Stick",
+        description: "Turn up the heat with our SHEGLAM Crystal Jelly Glaze Stick! This dazzling wonder combines water and glazing pearls to deliver a wet, lightweight, sparkling finish with a single swipe. With its unique solid-yet-jelly formula, you'll get a bouncy, crushed gemstone effect and a refreshing glass-like shine, just like the dazzle of a galaxy.",
+        price: 32.5,
+        quantity: 12,
+        category: "eye",
+        images: [p301, p302, p303, p304],
+        bestseller:false
+    },
+    
 
 
 ]
